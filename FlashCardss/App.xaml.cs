cd46ae1b44293175f1c5cad21303bbd574d9e445
kortay <kortaykokturk@gmail.com>
2025-05-15 18:47:22 +0300

    first commit Work in Progress

---
+++ FlashCardss/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FlashCardss
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
